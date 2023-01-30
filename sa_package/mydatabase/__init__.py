16e0c201b0a5151593e6ca329e0d5b97c2c153ed
SeungA <seungashin9275@gmail.com>
2023-01-30 16:24:57 +0900

    Update __init__.py

--- sa_package/mydatabase/__init__.py
+++ sa_package/mydatabase/__init__.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from sqlalchemy import create_engine
 
 
@@ -25,4 +24,58 @@ def get_engine(dbms:str, username:str, password:str, host:str, port:int, databas
     return engine
 
 
-# def upload_df(df:)
+def upload_df(dbms:str, engine, df, pk_list:list, scheme:str, table:str):
+
+    """
+    Parameter
+    -----------
+    dbms : {"mysql", "postgres"}
+
+    engine
+
+    df : pd.DataFrame()
+
+    pk_list : list of str
+
+    scheme : str
+
+    table : str
+    """
+
+    copy_df = df.copy()
+    
+    # NaN 값 처리
+    copy_df.fillna("", inplace=True)
+    for col in copy_df.columns:
+        if copy_df[col].dtypes in ['float64', 'int64']:
+            copy_df[col] = copy_df[col].astype(str)    
+
+    for col in copy_df.columns:
+        copy_df[col] = copy_df[col].apply(lambda x: None if x == "" else str(x))
+
+    if dbms == "mysql":
+        cols = ', '.join('`{0}`'.format(c) for c in copy_df.columns)
+        strings = ', '.join('%s' for i in copy_df.columns)
+        update_values = ', '.join('`{0}`=VALUES(`{0}`)'.format(c) for c in copy_df.drop(columns=pk_list).columns)
+        values = list(copy_df.itertuples(index=False, name=None))
+
+        sql = "INSERT INTO `" + scheme + "`.`" + table + "` ({0}) VALUES({1}) ON DUPLICATE KEY UPDATE {2};".format(cols, strings, update_values)
+
+    elif dbms == "postgres":
+        cols = ', '.join('"{0}"'.format(c) for c in copy_df.columns)
+        strings = ', '.join('%s' for i in copy_df.columns)
+        pk_values = ', '.join('"{0}"'.format(c) for c in pk_list)
+        update_values = ', '.join('"{0}"=EXCLUDED.{0}'.format(c) for c in copy_df.drop(columns=pk_list).columns)
+        values = list(copy_df.itertuples(index=False, name=None))
+
+        sql = 'INSERT INTO ' + scheme + '."' + table + '" ({0}) VALUES({1}) ON CONFLICT ({2}) DO UPDATE SET {3};'.format(cols, strings, pk_values, update_values)
+        
+    try:
+        with engine.connect() as connection:
+            connection.execute(sql, values)
+
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
