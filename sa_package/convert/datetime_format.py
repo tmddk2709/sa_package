8c1e3ff48768ea4c9c9fa254ebd726912eacb5ba
SeungA <seungashin9275@gmail.com>
2023-02-03 18:22:30 +0900

    Update datetime_format.py

--- sa_package/convert/datetime_format.py
+++ sa_package/convert/datetime_format.py
@@ -1,11 +1,20 @@
+from __future__ import annotations
+
 import time
 import datetime
 
-def convert_date_to_timestamp(target_date):
+
+def convert_datetime_to_timestamp(target_date:datetime.datetime | datetime.date):
     """
-    datetime.date 날짜를 타임스탬프로 변환
+    datetime.datetime 혹은 datetime.date 를 타임스탬프로 변환
     """
-    timestamp = int(time.mktime(datetime.datetime(
-        target_date.year, target_date.month, target_date.day, 9, 0).timetuple()))
+
+    assert isinstance(target_date, datetime.datetime) or isinstance(target_date, datetime.date), "잘못된 타입 입력"
+
+    if isinstance(target_date, datetime.datetime):
+        timestamp = int(time.mktime(datetime.datetime(target_date.year, target_date.month, target_date.day, target_date.hour, target_date.minute, target_date.second).timetuple()))
+
+    elif isinstance(target_date, datetime.date):
+        timestamp = int(time.mktime(datetime.datetime(target_date.year, target_date.month, target_date.day, 9, 0, 0).timetuple()))
 
     return timestamp
