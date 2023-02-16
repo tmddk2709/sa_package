4a6cd33c901623ac3cc31bf02c992d2baabda3d9
SeungA <seungashin9275@gmail.com>
2023-02-16 18:21:10 +0900

    Add convert_iso_8859_to_sec function

--- sa_package/convert/time_format.py
+++ sa_package/convert/time_format.py
@@ -45,3 +45,50 @@ def convert_hm_format_to_min(hm):
         mins = 0
 
     return float(hours)*60 + float(mins)
+
+
+
+def convert_iso_8859_to_sec(iso:str) -> int:
+    """
+    convert iso 8601 time format to seconds
+    """
+
+    sec = 0
+    cur_type = "date"
+    cur_num = ""
+
+    for c in iso:
+        if c == "P":
+            pass
+        elif c == "T":
+            cur_type = "time"
+
+        elif c in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            cur_num += c
+        
+        elif c == "Y":
+            sec += int(cur_num) * 365 * 24 * 3600
+            cur_num = ""
+
+        elif c == "M":
+            if cur_type == "date":
+                sec += int(cur_num) * 30 * 24 * 3600
+            else:
+                sec += int(cur_num) * 60
+
+            cur_num = ""
+        
+        elif c == "D":
+            sec += int(cur_num) * 24 * 3600
+            cur_num = ""
+
+        elif c == "H":
+            sec += int(cur_num) * 3600
+            cur_num = ""
+        
+        elif c == "S":
+            sec += int(cur_num)
+            cur_num = ""
+
+
+    return int(sec)
